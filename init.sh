66c24c54ababf689ee454ba830e4fc7e74e3da75
oigen <thr1@list.ru>
2016-03-09 18:17:04 +0700

    Django 1

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/
+sudo ln -sf /home/box/web/etc/guni-dj.conf.py /etc/gunicorn.d/
 sudo /etc/init.d/nginx restart
 
 
