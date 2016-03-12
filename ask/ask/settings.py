09f82643929183832234dc6f13836d814a84312f
oigen <thr1@list.ru>
2016-03-12 12:08:21 +0700

    Django MySQL - add Question, Answer

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -26,6 +26,11 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_DIRS = {
+    '/home/box/web/ask/templates',
+
+}
+
 
 # Application definition
 
@@ -36,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +63,13 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+    'ENGINE' : 'django.db.backends.mysql',
+	'NAME' : 'django_db',
+	'USER' : 'admin',
+	'PASSWORD' : '',
+	'HOST' : 'localhost',
+	'PORT':  ''
     }
 }
 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,18 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Question(models.Model):
-	title = models.CharField(max_length=255)
-	text = models.TextField()
-	added_at = models.DateField(blank=True)
-	rating = models.IntegerField()
-	author = models.CharField(max_length=255)
-	likes = models.
-	
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(blank=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    article_likes = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'question'
+
+
 class Answer(models.Model):
-	text = models.TextField()
-	added_at = models.DateField(blank=True)
-	question = models.IntegerField()
-	author = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(blank=True)
+    question = models.IntegerField()
+    author = models.ForeignKey(User)
+
+    class Meta:
+        db_table = 'answer'
+
 
